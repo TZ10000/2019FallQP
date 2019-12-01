81d802d996ffc63b099ff0d8d6ffb0946a09be03
Tianze Guan <gtz@tianzes-mbp.home>
2019-12-01 13:34:55 -0800

    pull version

--- Code/Javascript/index.js
+++ Code/Javascript/index.js
@@ -1,7 +1,8 @@
   console.log("Hello World!")
 
 function getCount() {
+    var counts = '{{=count}}';
     document.getElementById("countTag").textContent = "count is: 42";
-    console.log(42);
+    console.log(counts);
     return 42;
 }
