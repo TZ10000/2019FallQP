0a20af5125725b151d980dcc0845064790447fb4
Tianze Guan <gtz@Tianzes-MacBook-Pro.local>
2019-12-01 15:30:50 -0800

    ?

--- Code/Javascript/index.js
+++ Code/Javascript/index.js
@@ -10,20 +10,13 @@ function getCount() {
 
 function readTextFile(file)
 {
-    console.log("called");
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-                return allText;
-            }
-        }
-    }
-    rawFile.send(null);
+    // Requiring fs module in which  
+    // readFile function is defined. 
+    const fs = require('fs') 
+  
+    fs.readFile(file, (err, data) => { 
+        if (err) throw err; 
+  
+        console.log(data.toString()); 
+    }) 
 }
