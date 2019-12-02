112ca28c49d413816e476d64c041d247e48ea7cb
Tianze Guan <gtz@Tianzes-MacBook-Pro.local>
2019-12-01 16:05:59 -0800

    ?

--- Code/Javascript/index.js
+++ Code/Javascript/index.js
@@ -1,15 +1,13 @@
   console.log("Hello World!")
 
 function getCount() {
-    fetch('../data.json')
-        .then(result=>result.json())
-        .then(console.log);
+    
 
-    // const fs = require('fs');
+    const fs = require('fs');
 
-    // let rawdata = fs.readFileSync('count.json');
-    // let cnt = JSON.parse(rawdata);
-    // console.log(cnt);
+    let rawdata = fs.readFileSync('data.json');
+    let cnt = JSON.parse(rawdata);
+    console.log(cnt);
     
     document.getElementById("countTag").textContent = "count is: " + cnt;
 }
